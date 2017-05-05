22348f42a68c85a173bd6710af673fe3074de82a
ATOMIX\Snowy <snowy6@gmail.com>
2017-05-05 20:39:18 +0200

    Fixed issue with fire rate hud object

--- Source/Asteroids/Private/PlayerHUD.cpp
+++ Source/Asteroids/Private/PlayerHUD.cpp
@@ -19,9 +19,9 @@ void APlayerHUD::DrawBars() {
 		DrawRect(FLinearColor::Yellow, LocationX + 60, LocationY, BarWidth, BarHeight);
 		DrawRect(FLinearColor::Blue, LocationX + 61, LocationY + 1, (BarWidth - 2) * healthWidth, BarHeight - 2);
 
-		float rateWidth = 1-((playerShip->FireRate - playerShip->MinFireRate) / (playerShip->MaxFireRate - playerShip->MinFireRate));
+		float rateWidth = 1 - ((playerShip->FireRate - playerShip->MinFireRate) / (playerShip->MaxFireRate - playerShip->MinFireRate));
 		DrawText(TEXT("Fire Rate:"), FLinearColor::White, LocationX, LocationY + 20);
 		DrawRect(FLinearColor::Yellow, LocationX + 60, LocationY + 20, BarWidth, BarHeight);
-		DrawRect(FLinearColor::Red, LocationX + 61, LocationY + 21, (BarWidth - 2) * rateWidth, BarHeight - 2);
+		DrawRect(FLinearColor::Red, LocationX + 61, LocationY + 21, ((BarWidth - 2) * rateWidth), BarHeight - 2);
 	}
 }
